7d7c911aa91eb7d8bc9cd76b3577561ccbb84d5a
gulfeyzaciftci <gulfeyza333@gmail.com>
2023-03-08 19:24:25 +0100

    comp

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -8,13 +8,26 @@ import {
   SelectBox,
 } from "./Header.style";
 
-const Header = () => {
+const Header = ({ setQuery, setSelectType }) => {
+  const printType = ["all", "books", "magazines"];
   return (
     <HeaderContainer>
       <HeaderTitle>Books or Magazines</HeaderTitle>
       <HeaderForm>
-        <SearchInput type="text" placeholder="name" required />
-        <SelectBox>
+        <SearchInput
+          type="text"
+          placeholder="name"
+          onChange={(e) => setQuery(e.target.value)}
+          required
+        />
+        <SelectBox onChange={(e) => setSelectType(e.target.value)}>
+          {printType.map((type) => {
+            return (
+              <option key={type} value={type}>
+                {type}
+              </option>
+            );
+          })}
           <option>All</option>
         </SelectBox>
         <HeaderButton type="submit">Search</HeaderButton>
