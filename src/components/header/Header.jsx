39a5c95c10fdb441605b5a04cfdfa4e24b468a5b
gulfeyzaciftci <gulfeyza333@gmail.com>
2023-03-08 18:45:52 +0100

    first commit

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import {
+  HeaderButton,
+  HeaderContainer,
+  HeaderForm,
+  HeaderTitle,
+  SearchInput,
+  SelectBox,
+} from "./Header.style";
+
+const Header = () => {
+  return (
+    <HeaderContainer>
+      <HeaderTitle>Books or Magazines</HeaderTitle>
+      <HeaderForm>
+        <SearchInput type="text" placeholder="name" required />
+        <SelectBox>
+          <option>All</option>
+        </SelectBox>
+        <HeaderButton type="submit">Search</HeaderButton>
+      </HeaderForm>
+    </HeaderContainer>
+  );
+};
+
+export default Header;
---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { HomeContainer, HomeImg, MainContainer } from "./Home.style";
+import Header from "../../components/header/Header";
+import homeImg from "../../assets/books.jpg";
+
+const Home = () => {
+  return (
+    <HomeContainer>
+      <Header />
+
+      <HomeImg>
+        <img src={homeImg} alt="home image" />
+      </HomeImg>
+
+      <MainContainer></MainContainer>
+    </HomeContainer>
+  );
+};
+
+export default Home;
